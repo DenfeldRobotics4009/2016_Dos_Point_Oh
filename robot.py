c67ac8bcb6b1a6f9fd24f46b35774b58726ef8d0
nikolojedison <nikolojedison@gmail.com>
2016-02-06 11:49:26 -0600

    Right, no more USB camera:

--- robot.py
+++ robot.py
@@ -29,18 +29,6 @@ class Mantis(wpilib.SampleRobot):
         self.macroTimeout = self.oi.smart_dashboard.getInt("Macro", 15)
         Settings.num_macro_timeout = self.macroTimeout
 
-        #these are all actually really self explanatory. Wonder if they work.
-        self.camera = wpilib.USBCamera()
-        self.camera.setExposureManual(50)
-        self.camera.setBrightness(80)
-        self.camera.setFPS(15)
-        self.camera.setSize(160, 120)
-        self.camera.setWhiteBalanceAuto()
-        self.camera.updateSettings() #update with the new settings so it actually does what we want it to
-
-        server = wpilib.CameraServer.getInstance() #set up the camera server
-        server.startAutomaticCapture(self.camera) #start the camera server
-
     def autonomous(self):
         """Auton code."""
 
